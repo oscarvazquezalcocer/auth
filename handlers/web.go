f2cc7920db236214caec93e536d2c5c6ee84031c
Oscar Vazquez <0skr.vazquez@gmail.com>
2023-09-07 11:06:23 -0600

    Redirect url

--- handlers/web.go
+++ handlers/web.go
@@ -12,13 +12,14 @@ var (
 	googleOauthConfig = oauth2.Config{
 		ClientID:     "926578288193-101dm849obhuhdu460s1csr7e5h36j6u.apps.googleusercontent.com",
 		ClientSecret: "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",
-		RedirectURL:  "http://localhost:8081/callback",
-		Scopes:       []string{"openid", "email", "profile"},
-		Endpoint:     google.Endpoint,
+		//RedirectURL:  "http://localhost:8081/callback",
+		Scopes:   []string{"openid", "email", "profile"},
+		Endpoint: google.Endpoint,
 	}
 )
 
 func Login(c *gin.Context) {
+	googleOauthConfig.RedirectURL = c.DefaultQuery("redirect_uri", "/")
 	url := googleOauthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
 	c.Redirect(http.StatusFound, url)
 }
